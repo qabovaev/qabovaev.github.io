97a3ab4c7cf824c8b9ca34d69bc01b17c0d4b8fd
Igor Bovaev <106065940+qabovaev@users.noreply.github.com>
2022-08-17 18:44:14 +0300

    Update game.js

--- angryflappybird/game.js
+++ angryflappybird/game.js
@@ -1,79 +1,78 @@
-// Flappy Bird на JavaScript
-var cvs = document.getElementById("canvas"); // Холст
-var ctx = cvs.getContext("2d"); // Контекст рисования на холсте
 
-var bird = new Image(); // Объект птички
-var bg = new Image(); // Объект фон
-var fg = new Image(); // Объект земля
-var pipeUp = new Image(); // Объект труба сверху
-var pipeBottom = new Image(); // Объект труба снизу
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
 
-bird.src = "img/bird.png"; // Изображение птички
-bg.src = "img/bg.png"; // Изображение фон
-fg.src = "img/fg.png"; // Изображение земля
-pipeUp.src = "img/pipeUp.png"; // Изображение труба сверху
-pipeBottom.src = "img/pipeBottom.png"; // Изображение труба снизу
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeUp = new Image();
+var pipeBottom = new Image();
 
-var gap = 90; // Просвет между трубами
+bird.src = "img/bird.png";
+bg.src = "img/bg.png";
+fg.src = "img/fg.png";
+pipeUp.src = "img/pipeUp.png";
+pipeBottom.src = "img/pipeBottom.png";
 
-document.addEventListener("keydown", moveUp); // Вызов функции moveUp при нажатии клавиши
+var gap = 90;
 
+document.addEventListener("keydown", moveUp);
 function moveUp() {
-    yPos -= 30; // Подняться вверх на 30 пунктов
+    yPos -= 25;
 }
 
-// Массив труб
+
 var pipe = [];
 pipe[0] = {
-    x : cvs.width, // Позиционируем трубу по координате X
-    y : 0 // // Позиционируем трубу по координате Y
+    x : cvs.width,
+    y : 0 // //
 }
 
-var score = 0; // Счёт
-var xPos = 100; // Позиция птички по X
-var yPos = 150; // Позиция птички по Y
-var grav = 0.8; // Гравитация
+var score = 0;
+var xPos = 100;
+var yPos = 150;
+var grav = 0.8;
 
-// Функция отрисовки
+
 function draw() { 
     ctx.drawImage(bg, 0, 0); // Отрисовка фона
-    for (var i = 0; i < pipe.length; i++) { // Цикл отрисовки труб
-        // ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y); // Отрисовка верхней трубы
+    for (var i = 0; i < pipe.length; i++) { 
+        // ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
         let num = 10
-        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap); // Отрисовка нижней трубы
-        pipe[i].x -= num; // Движение труб на птичку и скорость движения
+        ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
+        pipe[i].x -= num; 
         
-        if (pipe[i].x == 5) { // Добавление новых труб при выполнении условия
+        if (pipe[i].x == 5) { 
             pipe.push({
                 x : cvs.width,
                 y : Math.floor(Math.random() * pipeUp.height) - pipeUp.height
             });
         }
 
-        // Отслеживание прикосновений к трубам
+        
         if (xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width
             &&
             (yPos + bird.height >= pipe[i].y + pipeUp.height + gap)
             || yPos + bird.height >= cvs.height - fg.height || yPos <= 0) {
-                location.reload(); // Перезагрузка страницы при прикосновении
+                location.reload(); 
             }
     
-            if (pipe[i].x == 5) { // Добавляем 1 к счёту при пролёте трубы
+            if (pipe[i].x == 5) { 
                 score++;
             }
         }
 
-    ctx.drawImage(fg, 0, cvs.height - fg.height); // Отрисовка земли
-    ctx.drawImage(bird, xPos, yPos); // Отрисовка птички
+    ctx.drawImage(fg, 0, cvs.height - fg.height); 
+    ctx.drawImage(bird, xPos, yPos); 
     
-    yPos += grav; // Под воздействием гравитации птичка падает вниз
+    yPos += grav; 
 
-    ctx.fillStyle = "#000"; // Цвет текста
-    ctx.font = "24px Verdana"; // Шрифт текста
-    ctx.fillText("Счет: " + score, 10, cvs.height - 20); // Счёт (позиционирование)
+    ctx.fillStyle = "#000"; 
+    ctx.font = "24px Verdana"; 
+    ctx.fillText("Счет: " + score, 10, cvs.height - 20); 
 
-    requestAnimationFrame(draw); // Запрашиваем кадр анимации
+    requestAnimationFrame(draw); 
 }
 
 
-pipeBottom.onload = draw; // Вызываем функцию draw после отрисовки нижней трубы
+pipeBottom.onload = draw; 
